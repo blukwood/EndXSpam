4b40a0d50fe13618c6914103499a8d51805d7d37
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-14 14:06:39 +0530

    Update vcraid.py

--- kabeer/modules/vcraid.py
+++ kabeer/modules/vcraid.py
@@ -10,20 +10,21 @@ from helpers.callmusic import callsmusic
 @app1.on_message(filters.command("alive", PREFIX))
 @app2.on_message(filters.command("alive", PREFIX))
 async def vcraid(client: Client, message: Message):
-  lel = await message.reply("Please Wait....")
-  audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
-  if audio:
-    file_name = get_file_name(audio)
-    file_path = await convert(
-      (await message.reply_to_message.download(file_name))
-      if not path.isfile(path.join("downloads", file_name)) else file_name
-    )
+    lel = await message.reply("Please Wait....")
+    audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
+    if audio:
+      file_name = get_file_name(audio)
+      file_path = await convert(
+        (await message.reply_to_message.download(file_name))
+        if not path.isfile(path.join("downloads", file_name)) else file_name
+      )
     else:
       return await lel.edit_text("Bsdk, Reply to Audio.")
+    
     if message.chat.id in callsmusic.pytgcalls.active_calls:
       await message.reply("Already Playing")
       return await lel.delete()
     else:
-        callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
-        await message.reply("Playing...")
-        return await lel.delete()
+      callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
+      await message.reply("Playing...")
+      return await lel.delete()
