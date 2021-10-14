cb5361142236c2b9842651b2336abd8d33bde057
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-14 12:58:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_HASH = os.getenv("API_HASH")
+API_ID = int(os.getenv("API_ID"))
+P1 = os.getenv("P1")
+P2 = os.getenv("P2")
+PREFIX = os.environ.get("PREFIX", ".")
+LOG_CHAT = int(os.getenv("LOG_CHAT"))
