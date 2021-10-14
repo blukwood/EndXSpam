756142fdde011252a9233ed8d25e60f2ef578b00
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-14 12:57:43 +0530

    Delete EndXSpam directory

--- EndXSpam/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN1 = os.environ.get("BOT_TOKEN1")
-APP_ID = int(os.environ.get("APP_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN2 = os.environ.get("BOT_TOKEN2")
-SUDO = int(os.environ.get("SUDO"))
