c03382617df4c21649a72c861ff7be28ee56128c
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-14 11:39:23 +0530

    Update start.py

--- EndXSpam/plugins/start.py
+++ EndXSpam/plugins/start.py
@@ -1,9 +1,10 @@
 from pyrogram import Client, filters
+from EndXSpam import SUDO
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Client.on_message(filters.command("start"))
 async def start(_, message: Message):
-    if e.sender_id in SMEX_USERS:
+    if e.sender_id in SUDO:
         await message.reply_text(
             f"""<b>Hi {message.from_user.first_name} 😉️!</b>
             """,
