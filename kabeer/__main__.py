260007b7354bc98e4c84f88dfec109519be86978
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-14 12:59:53 +0530

    Create __main__.py

---
+++ kabeer/__main__.py
@@ -0,0 +1,8 @@
+from pyrogram import idle, Client, filters
+from kabeer import app1, app2, LOGGER
+from kabeer.modules import *
+import os
+
+print("Pyrogram User Client is Starting... \nPowered By @TheCodents\n(C) 2021 Jayant Kageri\n" )
+app1.run()
+app2.run()
