7818adc46c69e9cad1fbde2a2cf651a8a2610499
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-14 11:41:10 +0530

    Update __init__.py

--- EndXSpam/__init__.py
+++ EndXSpam/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-    BOT_TOKEN1 = os.environ.get("BOT_TOKEN1", "")
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN2 = os.environ.get("BOT_TOKEN2", "")
-    SUDO = os.environ.get("BOT_TOKEN2", "")
+    BOT_TOKEN1 = os.environ.get("BOT_TOKEN1")
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN2 = os.environ.get("BOT_TOKEN2")
+    SUDO = os.environ.get("BOT_TOKEN2")
